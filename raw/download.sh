eb282e83928dcc41801bc386d18e4c10ac45b4e0
sacha <sacha@labsquare.org>
2021-11-24 11:43:45 +0100

    add missingno and download last ensembl gene set

--- raw/download.sh
+++ raw/download.sh
@@ -10,10 +10,18 @@ echo "Download omim"
 wget -nc https://www.omim.org/static/omim/data/mim2gene.txt
 wget -nc https://raw.githubusercontent.com/materechm/plabData/master/yeast_replaceable_genes/OMIM/genemap2.txt
 
+echo "Download Ensembl gene"
+wget -nc http://ftp.ensembl.org/pub/release-92/gtf/homo_sapiens/Homo_sapiens.GRCh38.92.gtf.gz
 
 # Compute gene_map 
 
 python create_gene_map.py > gene_map.csv 
+
+zcat Homo_sapiens.GRCh38.92.gtf.gz|
+grep -v "#"|
+awk 'BEGIN{OFS="\t"; print("ensembl_id", "gene_name","start","end","type")} $3 == "gene"{print $10, $14,$4,$5,$18}'|
+sed "s/[\";]//g" > ensembl_gene.tsv
+
 
 
 # Compute hash 
